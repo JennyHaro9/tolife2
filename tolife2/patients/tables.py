89f44e27b8cb6423180416487283799c6bcf9900
Jenny Haro <jennyharo_19@hotmail.com>
2025-06-23 15:45:55 +0200

    feat: add template to show patients

--- tolife2/patients/tables.py
+++ tolife2/patients/tables.py
@@ -1,11 +1,29 @@
 import django_tables2 as tables
-from django_tables2.utils import A
 
 
 class PatientTable(tables.Table):
-    name = tables.Column()
-    identifier = tables.LinkColumn("patients:patient_detail", args=[A("id")])
-    doctor = tables.Column()
+    name = tables.Column(verbose_name="Name")
+    identifier = tables.Column(verbose_name="Identifier")
+    doctor = tables.Column(verbose_name="Doctor")
+    actions = tables.TemplateColumn(
+        verbose_name="",
+        extra_context={"patient_detail_url": "patients:patient_detail"},
+        template_code="""
+    <button
+  class="btn btn-outline-primary"
+  hx-get="{% url patient_detail_url record.id %}"
+  hx-target="#table-wrapper"
+  hx-swap="innerHTML"
+  title="Mostrar gráfica de parámetros"
+>
+  <i class="bi bi-bar-chart-fill"></i>
+</button>
+    """,
+    )
+
+    def __init__(self, **kwargs):
+        self.hx_table_url = kwargs.pop("hx_table_url")
+        super().__init__(**kwargs)
 
     class Meta:
         fields = ("name", "identifier", "doctor")
--- tolife2/patients/urls.py
+++ tolife2/patients/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
+from .views import patient_detail_view
 from .views import patient_list_view
 
 app_name = "patients"
 
 urlpatterns = [
     path("", patient_list_view, name="patient_list"),
-    path("<pk>/", patient_list_view, name="patient_detail"),
+    path("<pk>/", patient_detail_view, name="patient_detail"),
 ]
